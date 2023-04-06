c60cf8b7760847f4710010905b30e17bc6135c40
camilo <camiloagudelo090@gmail.com>
2023-04-06 18:41:45 -0500

    añadiendo caja de comentarios a los post

--- resources/views/posts/mostrarunpost.blade.php
+++ resources/views/posts/mostrarunpost.blade.php
@@ -8,5 +8,32 @@ Post: {{$post->titulo}}
 
 
 @section('contenido')
+<div class="container">
+   
+    <div class="row">
+      <div class="col4">
+            <h6>{{$post->titulo}}</h6>
+            <img height="100px;" src="{{asset('uploads').'/'.$post->imagen}}" alt="">
+            <div>
+                <p>8 Likes</p>
+            </div>
+            <div>
+                 <!-- Aca aplicamos la relacion de eloquent de post contra user -->
+                <p>Creador del Post: {{$post->user->username}}</p>  
+                <br>
+                <p>fecha actualización: {{ $post->updated_at->diffForHumans() }}</p>
+                <br>
+                <p>Descripcion: {{$post->descripcion}}</p>
 
+               
+            </div>
+      </div>
+      <div class="col-2">
+        <!--Espacio en blanco -->
+      </div>
+      <div class="col-6">
+        comentarios
+      </div>
+    </div>
+  </div>
 @endsection
