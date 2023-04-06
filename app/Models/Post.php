c50ef337edbbba9a26bedb6e14442109a2aab5eb
camilo <camiloagudelo090@gmail.com>
2023-04-06 16:49:22 -0500

    almacenar post con relacion

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,6 +17,6 @@ class Post extends Model
     ];
     //relacion 1 a 1 1 post debe tener un usuario belong to a la inversa
     public function user(){
-        return  $this->belongsTo(User::class);
+        return  $this->belongsTo(User::class)->select(['name','username']);
     }
 }
